eeb8cfa353702e7a52e1973683ae4b2b10c62308
SKZ <skz@apple.com>
2024-07-22 13:51:13 +1000

    store search engine urls in json object

---
+++ src/data/searchEngineUrls.ts
@@ -0,0 +1,8 @@
+// List of search engine and url
+type searchEnginerUrlTypes = {
+  [key: string]: string
+}
+export const searchEngineUrls: searchEnginerUrlTypes = {
+  google: 'https://infotrack-tests.infotrack.com.au/Google/',
+  bing: 'https://infotrack-tests.infotrack.com.au/Bing/'
+}
